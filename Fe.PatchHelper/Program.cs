1e187567dbf679fc5f70c3c459baa5c9dd6e51eb
Antidale <antidale@tellah.life>
2025-08-14 16:38:45 -0700

    let's clear out my own file structure

--- Fe.PatchHelper/Program.cs
+++ Fe.PatchHelper/Program.cs
@@ -1,13 +1,8 @@
 ﻿
 using Fe.PatchHelper;
 
-//4.6.0.anti
-// var testPath = "/Users/antidale/Downloads/FF4FE.bBAYAAAAAAAAAAAAABCQAAAAAAAAAACAABEAAUYAPAIA.SR31CWFQ9B.smc";
-//5.0.0.alpha
-// var testPath = "/Users/antidale/Downloads/FF4FE.cBQAC8PkZIioyOgKaGAwGg8FgMBgMBlJUPVUABTAYCABDcAABkOEAAQgABYAB2AMwARADYAhwC4gi.AFNP4KYGF3.smc";
-// 0.3.6 (?)
-var testPath = "/Users/antidale/Downloads/FF4FE.bAAMGIaw4AMgC6F4dFg.YCVE7XTDHA.smc";
 
-var success = SeedReader.TryGetSeedMetadata(testPath, out var jsonDoc);
+
+var success = SeedReader.TryGetSeedMetadata("testPath", out var jsonDoc);
 
 Console.WriteLine(success ? jsonDoc.ToString() : "failed to read metadata");
