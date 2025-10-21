8d2f0c910728a728b9939dbabcfdc9453fb1c4f1
Antidale <antidale@tellah.life>
2025-10-21 11:30:52 -0700

    actually adding the basics of a create-patch command

--- Fe.PatchHelper/Commands/CreatePatchCommand.cs
+++ Fe.PatchHelper/Commands/CreatePatchCommand.cs
@@ -1,26 +1,23 @@
 using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Fe.PatchHelper.Commands;
 
-public class GetFileMetadataCommand : Command<GetFileMetadataCommand.Settings>
+public class CreatePatchCommand : Command<CreatePatchCommand.Settings>
 {
     public class Settings : CommandSettings
     {
-        [Description("path, including file name, to the file you want to pull FE metadata from")]
+        [Description("path, including file name, to the file you want to recreate a patch page from")]
         [CommandArgument(0, "<filePath>")]
         public required string FilePath { get; init; }
     }
 
     public override int Execute(CommandContext context, [NotNull] Settings settings)
     {
-        AnsiConsole.WriteLine(SeedReader.TryGetSeedMetadata(settings.FilePath, out var seedMetadata)
-            ? seedMetadata.ToString()
-            : $"Could not parse metadata for {settings.FilePath}");
-
+        return 0;
+        throw new NotImplementedException();
         return 0;
     }
 }
